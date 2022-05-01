43813db8eabcd030295ae0acbabd895185af4335
Dylan K. Taylor <dktapps@pmmp.io>
2022-05-01 14:36:25 +0100

    Strip unwanted files

--- tests/android/static.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OUTPUT=$(readelf -d "$PHP_BINARIES/bin/php")
-
-if [ "$OUTPUT" != "There is no dynamic section in this file." ]; then
-	exit 1
-fi
-
-exit 0
--- tests/generic/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OUTPUT=$("$PHP_BINARIES" -r 'echo 1;')
-
-if [ "$OUTPUT" != "1" ]; then
-	exit 1
-fi
-
-exit 0
